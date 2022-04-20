b0384f5aa567685188a2fb90e9c4ec77765a009b
Marc Bastian Heinrichs <typo3@mbh-software.de>
2022-04-20 15:03:55 +0200

    [TASK] Add more options

--- Classes/Routing/Aspect/PersistedAliasMapper.php
+++ Classes/Routing/Aspect/PersistedAliasMapper.php
@@ -6,9 +6,11 @@ namespace Mabahe\MbhPersistedaliasmapper\Routing\Aspect;
 
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Database\Query\Restriction\EndTimeRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendGroupRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendRestrictionContainer;
 use TYPO3\CMS\Core\Database\Query\Restriction\HiddenRestriction;
+use TYPO3\CMS\Core\Database\Query\Restriction\StartTimeRestriction;
 use TYPO3\CMS\Core\Routing\Aspect\PersistedAliasMapper as CorePersistedAliasMapper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
@@ -44,8 +46,14 @@ class PersistedAliasMapper extends CorePersistedAliasMapper
         // Frontend Groups are not available at this time (initialized via TSFE->determineId)
         // So this must be excluded to allow access restricted records
         $queryBuilder->getRestrictions()->removeByType(FrontendGroupRestriction::class);
-        if ($this->settings['includeHidden']) {
+        if (isset($this->settings['includeHidden']) && $this->settings['includeHidden'] === true) {
             $queryBuilder->getRestrictions()->removeByType(HiddenRestriction::class);
+        }
+        if (isset($this->settings['ignoreStartTime']) && $this->settings['ignoreStartTime'] === true) {
+            $queryBuilder->getRestrictions()->removeByType(StartTimeRestriction::class);
+        }
+        if (isset($this->settings['ignoreEndTime']) && $this->settings['ignoreEndTime'] === true) {
+            $queryBuilder->getRestrictions()->removeByType(EndTimeRestriction::class);
         }
         return $queryBuilder;
     }
